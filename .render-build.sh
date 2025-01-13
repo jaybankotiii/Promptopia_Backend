fa5e5905eb3f07534163a78112cb0244eff20f03
jaybankotiii <jay.bankoti@trilogy.com>
2025-01-13 12:38:47 +0530

    Fixed python problem

--- .render-build.sh
+++ .render-build.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
-# Install Python dependencies in a virtual environment
+
+# Install Node.js dependencies
+npm install
+
+# Set up Python virtual environment and install dependencies
 python3 -m venv venv
 source venv/bin/activate
 pip install --no-cache-dir -r requirements.txt
